0fe8c21cc72e325790b3b79b76cfb4501476daa7
Keza <144003584+jkeza1@users.noreply.github.com>
2025-01-22 15:56:59 +0200

    across_the_planes

--- maths/linear_algebra/5-across_the_planes.py
+++ maths/linear_algebra/5-across_the_planes.py
@@ -1,21 +1,35 @@
 #!/usr/bin/env python3
 """
-    adds two matrices element-wise.
+add_matrices2D = __import__('5-across_the_planes').add_matrices2D
+
+mat1 = [[1, 2], [3, 4]]
+mat2 = [[5, 6], [7, 8]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
 """
 
 
 def add_matrices2D(mat1, mat2):
+    """Adds two 2D matrices element-wise.
+
+    Args:
+        mat1: A 2D list (matrix) of integers or floats.
+        mat2: A 2D list (matrix) of integers or floats.
+
+    Returns:
+        A new 2D list with the element-wise sum of mat1 and mat2.
+        If mat1 and mat2 are not the same shape, returns None.
     """
-    mat1: list of lists of ints/floats
-    mat2: list of lists of ints/floats
-    """
-    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
-        return [
-            [
-                mat1[i][j] + mat2[i][j]
-                for j in range(len(mat1[0]))
-            ]
-            for i in range(len(mat1))
-        ]
-    else:
+    if len(mat1) != len(mat2):
         return None
+
+    for row1, row2 in zip(mat1, mat2):
+        if len(row1) != len(row2):
+            return None
+
+    return [
+        [row1[i] + row2[i] for i in range(len(row1))]
+        for row1, row2 in zip(mat1, mat2)
+    ]
