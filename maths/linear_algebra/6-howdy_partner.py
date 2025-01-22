3cb8dff6dd26ad13ba8f72b52e3f86e80abbe985
Keza <144003584+jkeza1@users.noreply.github.com>
2025-01-22 15:57:56 +0200

    howdy_partner

--- maths/linear_algebra/6-howdy_partner.py
+++ maths/linear_algebra/6-howdy_partner.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """
-concatenates two arrays.
+cat_arrays = __import__('6-howdy_partner').cat_arrays
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+print(cat_arrays(arr1, arr2))
+print(arr1)
+print(arr2)
 """
 
 
 def cat_arrays(arr1, arr2):
-    """
-    arr1: list of ints/floats
-    arr2: list of ints/floats
+    """Concatenates two arrays.
+
+    Args:
+        arr1: A list of integers or floats.
+        arr2: A list of integers or floats.
+
+    Returns:
+        A new list that is the concatenation of arr1 and arr2.
     """
     return arr1 + arr2
