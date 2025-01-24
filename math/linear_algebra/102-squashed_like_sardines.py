b76ff4c7e2cb1a4184c4f502b4cb0c61676fb50f
Keza <144003584+jkeza1@users.noreply.github.com>
2025-01-24 22:22:07 +0200

    feat: added first line

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,4 +1,5 @@
- function that concatenates two matrices along a specific axis """
+#!/usr/bin/env python3
+""" defines function that concatenates two matrices along a specific axis """
 
 
 def matrix_shape(matrix):
@@ -22,6 +23,14 @@ def cat_matrices(mat1, mat2, axis=0):
             if shape1[i] != shape2[i]:
                 return None
     return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """ recursively calls function until gets to level to extend """
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1deepcopy(mat2), axis, 0)
 
 
 def rec(m1, m2, axis=0, current=0):
