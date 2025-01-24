962fdb19b4856213b6dea5edd37f4e9dbedd8133
Keza <144003584+jkeza1@users.noreply.github.com>
2025-01-24 22:45:06 +0200

    feat: done

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -30,12 +30,4 @@ def rec(m1, m2, axis=0, current=0):
     if axis != current:
         return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
     m1.extend(m2)
-    return m1deepcopy(mat2), axis, 0)
-
-
-def rec(m1, m2, axis=0, current=0):
-    """ recursively calls function until gets to level to extend """
-    if axis != current:
-        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
-    m1.extend(m2)
     return m1
