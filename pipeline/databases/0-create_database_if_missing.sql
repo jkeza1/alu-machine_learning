5fc8f7227f8076e5ba1514b817d76fb47fb3d13f
Keza <144003584+jkeza1@users.noreply.github.com>
2025-07-06 22:55:07 +0200

    create_database_if_missing

--- pipeline/databases/0-create_database_if_missing.sql
+++ pipeline/databases/0-create_database_if_missing.sql
@@ -1,6 +1,2 @@
--- connect to the mysql server 
--- once connected, use your credentials to log in
--- for example: mysql -u root -p
--- once logged in, run the following command to create the database
-
-CREATE DATABASE IF NOT EXISTS db_0;
+-- create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS db_0
